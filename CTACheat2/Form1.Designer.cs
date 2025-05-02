c40980010ded17f8a2c4ea92a077d7bd346b8983
javascript <javascriptcoolz@gmail.com>
2025-05-02 19:31:00 +0300

    Add project files.

---
+++ CTACheat2/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace CTACheat2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            GameSelectionBox = new ComboBox();
+            button2 = new Button();
+            StatusLabel = new Label();
+            ActionSelectionBox = new ComboBox();
+            ValueBox = new TextBox();
+            button3 = new Button();
+            BuildDateLabel = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(439, 277);
+            button1.Name = "button1";
+            button1.Size = new Size(91, 35);
+            button1.TabIndex = 0;
+            button1.Text = "Cheat it";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // GameSelectionBox
+            // 
+            GameSelectionBox.FormattingEnabled = true;
+            GameSelectionBox.Location = new Point(12, 12);
+            GameSelectionBox.Name = "GameSelectionBox";
+            GameSelectionBox.Size = new Size(121, 23);
+            GameSelectionBox.TabIndex = 1;
+            GameSelectionBox.Text = "Select a game...";
+            GameSelectionBox.SelectedIndexChanged += GameSelectionBox_SelectedIndexChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(439, 236);
+            button2.Name = "button2";
+            button2.Size = new Size(91, 35);
+            button2.TabIndex = 2;
+            button2.Text = "License...";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // StatusLabel
+            // 
+            StatusLabel.AutoSize = true;
+            StatusLabel.Location = new Point(12, 277);
+            StatusLabel.Name = "StatusLabel";
+            StatusLabel.Size = new Size(105, 15);
+            StatusLabel.TabIndex = 3;
+            StatusLabel.Text = "Detecting games...";
+            // 
+            // ActionSelectionBox
+            // 
+            ActionSelectionBox.FormattingEnabled = true;
+            ActionSelectionBox.Location = new Point(12, 41);
+            ActionSelectionBox.Name = "ActionSelectionBox";
+            ActionSelectionBox.Size = new Size(121, 23);
+            ActionSelectionBox.TabIndex = 4;
+            ActionSelectionBox.Text = "Select an action...";
+            ActionSelectionBox.SelectedIndexChanged += ActionSelectionBox_SelectedIndexChanged;
+            // 
+            // ValueBox
+            // 
+            ValueBox.Enabled = false;
+            ValueBox.Location = new Point(12, 94);
+            ValueBox.Name = "ValueBox";
+            ValueBox.PlaceholderText = "Type in a value...";
+            ValueBox.Size = new Size(201, 23);
+            ValueBox.TabIndex = 5;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(337, 11);
+            button3.Name = "button3";
+            button3.Size = new Size(193, 23);
+            button3.TabIndex = 6;
+            button3.Text = "Open-source acknowledgements";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // BuildDateLabel
+            // 
+            BuildDateLabel.AutoSize = true;
+            BuildDateLabel.Location = new Point(13, 299);
+            BuildDateLabel.Name = "BuildDateLabel";
+            BuildDateLabel.Size = new Size(59, 15);
+            BuildDateLabel.TabIndex = 7;
+            BuildDateLabel.Text = "Loading...";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(542, 324);
+            Controls.Add(BuildDateLabel);
+            Controls.Add(button3);
+            Controls.Add(ValueBox);
+            Controls.Add(ActionSelectionBox);
+            Controls.Add(StatusLabel);
+            Controls.Add(button2);
+            Controls.Add(GameSelectionBox);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "CTACheat2";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private ComboBox GameSelectionBox;
+        private Button button2;
+        private Label StatusLabel;
+        private ComboBox ActionSelectionBox;
+        private TextBox ValueBox;
+        private Button button3;
+        private Label BuildDateLabel;
+    }
+}
---
+++ CTACheat2/Form1.cs
@@ -0,0 +1,202 @@
+using IniParser;
+using IniParser.Model;
+using System.Data;
+
+namespace CTACheat2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BuildDateLabel.Text = "Built on " + CTACheat2.Properties.Resources.BuildDate;
+            string username = Environment.GetEnvironmentVariable("USERNAME");
+            int GameTally = 0;
+            if (Directory.Exists("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Boi"))
+            {
+                GameSelectionBox.Items.Add("CTA 1 (original)");
+                GameTally += 1;
+            }
+            if (Directory.Exists("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Bizarre_Adventure"))
+            {
+                GameSelectionBox.Items.Add("CTA 2 (original)");
+                GameTally += 1;
+            }
+            if (Directory.Exists("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Boi_Journey"))
+            {
+                GameSelectionBox.Items.Add("CTA 3 (original)");
+                GameTally += 1;
+            }
+            if (Directory.Exists("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Boi_Tuna"))
+            {
+                GameSelectionBox.Items.Add("CTA 4 (original)");
+                GameTally += 1;
+            }
+            StatusLabel.Text = "Found save files for " + GameTally.ToString() + " games.";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LicenseBox licenseBox = new LicenseBox();
+            licenseBox.ShowDialog();
+        }
+
+        private void GameSelectionBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (GameSelectionBox.Text)
+            {
+                case "CTA 1 (original)":
+                    string[] actions = { "Change stage" };
+                    ActionSelectionBox.Items.AddRange(actions);
+                break;
+                case "CTA 2 (original)":
+                    string[] actions2 = { "Change stage" };
+                    ActionSelectionBox.Items.AddRange(actions2);
+                break;
+                case "CTA 4 (original)":
+                    string[] actions4 = { "Set post bad ending", "Set bad ending", "Change stage" };
+                    ActionSelectionBox.Items.AddRange(actions4);
+                break;
+            }
+        }
+
+        private void ActionSelectionBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (ActionSelectionBox.Text)
+            {
+                case "Change stage":
+                    switch (GameSelectionBox.Text)
+                    {
+                        case "CTA 1 (original)":
+                            ValueBox.PlaceholderText = "Type in a stage from 1 to 4";
+                        break;
+                        case "CTA 2 (original)":
+                            ValueBox.PlaceholderText = "Type in a stage from 1 to 4";
+                        break;
+                        case "CTA 4 (original)":
+                            ValueBox.PlaceholderText = "Type in a stage from 1 to 4";
+                        break;
+                    }
+                    ValueBox.Enabled = true;
+                break;
+                default:
+                    ValueBox.PlaceholderText = "Type in a value";
+                    ValueBox.Enabled = false;
+                break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var parser = new FileIniDataParser();
+            string username = Environment.GetEnvironmentVariable("USERNAME");
+            switch(GameSelectionBox.Text)
+            {
+                case "CTA 1 (original)":
+                    if (ActionSelectionBox.Text == "Change stage")
+                    {
+                        try
+                        {
+                            Int32.Parse(ValueBox.Text);
+                        } catch(Exception c)
+                        {
+                            MessageBox.Show("Unknown exception while parsing value: " + c.Message, "CTACheat: Error on modification", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                        if (Int32.Parse(ValueBox.Text) > 4 || Int32.Parse(ValueBox.Text) < 1)
+                        {
+                            MessageBox.Show("Illegal argument: Stage cannot be more than 4 or less than 1!", "CTACheat: Error on modification", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        } 
+                        
+                        IniData saveFileData = parser.ReadFile("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Boi\\progress.ini");
+
+                        saveFileData["progress"]["stage"] = ValueBox.Text.ToString();
+
+                        parser.WriteFile("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Boi\\progress.ini", saveFileData);
+                    }
+                break;
+                case "CTA 2 (original)":
+                    if(ActionSelectionBox.Text == "Change stage")
+                    {
+                        try
+                        {
+                            Int32.Parse(ValueBox.Text);
+                        } catch (Exception c)
+                        {
+                            MessageBox.Show("Unknown exception while parsing value: " + c.Message, "CTACheat: Error on modification", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        if (Int32.Parse(ValueBox.Text) > 4 || Int32.Parse(ValueBox.Text) < 1)
+                        {
+                            MessageBox.Show("Illegal argument: Stage cannot be more than 4 or less than 1!", "CTACheat: Error on modification", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+
+                        IniData saveFileData = parser.ReadFile("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Boi_Journey\\hello.ini");
+
+                        saveFileData["progress"]["stage"] = ValueBox.Text.ToString();
+
+                        parser.WriteFile("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Boi_Journey\\hello.ini", saveFileData);
+                    }
+                break;
+                case "CTA 4 (original)":
+                    switch(ActionSelectionBox.Text)
+                    {
+                        case "Set post bad ending":
+                            IniData saveFileData = parser.ReadFile("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Boi_Tuna\\file.ini");
+
+                            if(saveFileData["Catto"]["PostBadEnd"] == "1")
+                            {
+                                saveFileData["Catto"]["PostBadEnd"] = "0";
+                            } else
+                            {
+                                saveFileData["Catto"]["PostBadEnd"] = "1";
+                            }
+
+                            parser.WriteFile("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Boi_Tuna\\file.ini", saveFileData);
+                        break;
+                        case "Set bad ending":
+                            IniData saveFileData2 = parser.ReadFile("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Boi_Tuna\\file.ini");
+
+                            if (saveFileData2["Catto"]["BadEnd"] == "1")
+                            {
+                                saveFileData2["Catto"]["BadEnd"] = "0";
+                            }
+                            else
+                            {
+                                saveFileData2["Catto"]["BadEnd"] = "1";
+                            }
+
+                            parser.WriteFile("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Boi_Tuna\\file.ini", saveFileData2);
+                        break;
+                        case "Change stage":
+                            try
+                            {
+                                Int32.Parse(ValueBox.Text);
+                            }
+                            catch (Exception c)
+                            {
+                                MessageBox.Show("Unknown exception while parsing value: " + c.Message, "CTACheat: Error on modification", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                            if (Int32.Parse(ValueBox.Text) > 4 || Int32.Parse(ValueBox.Text) < 1)
+                            {
+                                MessageBox.Show("Illegal argument: Stage cannot be more than 4 or less than 1!", "CTACheat: Error on modification", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                return;
+                            }
+
+                            IniData saveFileData3 = parser.ReadFile("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Boi_Tuna\\file.ini");
+
+                            saveFileData3["progress"]["stage"] = ValueBox.Text.ToString();
+
+                            parser.WriteFile("C:\\Users\\" + username + "\\AppData\\Local\\Catto_Boi_Tuna\\file.ini", saveFileData3);
+                        break;
+                    }
+                break;
+            }
+        }
+    }
+}
